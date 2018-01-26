f83789bae687deca9d1acfd655aff4f77ff5f643
Eric Helin <erichelin@quickenloans.com>
2018-01-26 12:43:04 -0500

    start on using the simple linear regression coefficient technique for multiple parameters...still needs work

--- src/javascript/machineLearning/multipleVariableLinearRegression/data.js
+++ src/javascript/machineLearning/multipleVariableLinearRegression/data.js
@@ -1,12 +1,12 @@
 function getGradientDescentDataSetOne() {
     const dataSets =  [
         [
-            {x: 'x', y: 'y', line: 'line'},
-            {x: 1,y: 1, line: 0},
-            {x: 2,y: 3, line: 0},
-            {x: 4,y: 3, line: 0},
-            {x: 3,y: 2, line: 0},
-            {x: 5,y: 5, line: 0},
+            {x1: 'x', y: 'y', line: 'line', xCount: 0},
+            {x1: 1,y: 1, line: 0, xCount: 1},
+            {x1: 2,y: 3, line: 0, xCount: 1},
+            {x1: 4,y: 3, line: 0, xCount: 1},
+            {x1: 3,y: 2, line: 0, xCount: 1},
+            {x1: 5,y: 5, line: 0, xCount: 1},
         ],
         //predictions
         [
@@ -39,17 +39,17 @@ function getGradientDescentDataSetOne() {
 function getGradientDescentDataSetTwo() {
     const dataSets =  [
         [
-            {x1: 'milesTraveled', x2: 'numDeliveries', x3: 'gasPrice', line: 'line'},
-            {x1: 89, x2: 4, x3: 3.84, line: 0},
-            {x1: 66, x2: 1, x3: 3.19, line: 0},
-            {x1: 78, x2: 3, x3: 3.78, line: 0},
-            {x1: 111, x2: 6, x3: 3.89, line: 0},
-            {x1: 44, x2: 1, x3: 3.57, line: 0},
-            {x1: 77, x2: 3, x3: 3.57, line: 0},
-            {x1: 80, x2: 3, x3: 3.03, line: 0},
-            {x1: 66, x2: 2, x3: 3.51, line: 0},
-            {x1: 109, x2: 5, x3: 3.54, line: 0},
-            {x1: 76, x2: 3, x3: 3.25, line: 0},
+            {x1: 'milesTraveled', x2: 'numDeliveries', x3: 'gasPrice', line: 'line', xCount: 0},
+            {x1: 89, x2: 4, x3: 3.84, line: 0, xCount: 3},
+            {x1: 66, x2: 1, x3: 3.19, line: 0, xCount: 3},
+            {x1: 78, x2: 3, x3: 3.78, line: 0, xCount: 3},
+            {x1: 111, x2: 6, x3: 3.89, line: 0, xCount: 3},
+            {x1: 44, x2: 1, x3: 3.57, line: 0, xCount: 3},
+            {x1: 77, x2: 3, x3: 3.57, line: 0, xCount: 3},
+            {x1: 80, x2: 3, x3: 3.03, line: 0, xCount: 3},
+            {x1: 66, x2: 2, x3: 3.51, line: 0, xCount: 3},
+            {x1: 109, x2: 5, x3: 3.54, line: 0, xCount: 3},
+            {x1: 76, x2: 3, x3: 3.25, line: 0, xCount: 3},
         ],
         //predictions
         [
--- src/javascript/machineLearning/multipleVariableLinearRegression/multipleVariableLinearRegressionAlgorithmOneDriver.js
+++ src/javascript/machineLearning/multipleVariableLinearRegression/multipleVariableLinearRegressionAlgorithmOneDriver.js
@@ -3,22 +3,91 @@ const dataSetSource = require('./data');
 function demoMultipleVariableLinearRegression(dataSetNumber) {
     console.log('inside var demoMultipleVariableLinearRegression - Algorithm 1 - Dataset' + dataSetNumber);
 
+    var dataSets = null;
+
     if (dataSetNumber === 1) {
-        simpleLinearRegressionCoefficientCalculation();
+        dataSets = dataSetSource.getGradientDescentDataSetOne();
+        linearRegressionCoefficientCalculation(dataSets);
+    } else if (dataSetNumber === 2) {
+        dataSets = dataSetSource.getGradientDescentDataSetTwo();
     }
+
+    // if (dataSetNumber === 1) {
+    //     simpleLinearRegressionCoefficientCalculation();
+    // }
 
     return 'inside var demoMultipleVariableLinearRegression - Algorithm 1 - Dataset' + dataSetNumber;
 }
-function simpleLinearRegressionCoefficientCalculation() {
-    let B0 = 0;
-    let B1 = 0;
-    let error = 0;   //is error cummulative or 0 for each iteration?
-    const alpha = .01;  //aka learning rate
-
-    let dataSets = dataSetSource.getGradientDescentDataSetOne();
+function linearRegressionCoefficientCalculation(dataSets) {
     const coefficients = calculateSimpleLinearRegressionCoefficients(dataSets[0]);
 
     makePrediction(coefficients, dataSets[1]);
+}
+function calculateSimpleLinearRegressionCoefficients(dataSets) {
+    let error = 0;   //is error cummulative or 0 for each iteration?
+    const alpha = .01;  //aka learning rate
+
+    const coefficients = getCoefficientArray(dataSets);
+
+    let interactionCnt = 2;
+    while(true) {
+
+        if (interactionCnt>22) {
+            break;
+        }
+
+        for(let outer=0; outer<dataSets.length; outer++) {
+            const currentDataset = dataSets[outer];
+
+            // TODO - handle multiple x1, x2, etc.
+            var xNan = isNaN(parseInt(currentDataset.x1));
+            var yNan = isNaN(parseInt(currentDataset.y));
+
+            const currentCoefficientTotal = getPValue(coefficients);
+
+            for(let inner=0; inner<coefficients.length; inner++) {
+
+                if (!xNan && !yNan) {
+
+                    // TODO - handle multiple x1, x2, etc.
+                    let p = currentCoefficientTotal * currentDataset.x1;
+                    error = p - currentDataset.y;
+
+                    if (inner === 0) {
+                        coefficients[inner] = coefficients[inner] - alpha * error;
+                    } else {
+
+                        // TODO - handle multiple x1, x2, etc.
+                        coefficients[inner] = coefficients[inner] - alpha * error * currentDataset.x1;
+                    }
+                }
+
+                interactionCnt++;
+            }
+        }
+    }
+
+    return coefficients;
+}
+function getPValue(coefficients) {
+    let curCoefficientTotal = 0;
+
+    for(let pValCtr=0; pValCtr<coefficients.length; pValCtr++) {
+        curCoefficientTotal += coefficients[pValCtr]
+    }
+
+    return curCoefficientTotal;
+}
+function getCoefficientArray(dataSets) {
+    const coefficients = [];
+
+    // TODO - make 'discoverable' by the number of x values (i.e. xCount=?)...how make BX inside json Object?
+    coefficients.push(0);
+    coefficients.push(0);
+    //coefficients.push({B2: 0});
+    //coefficients.push({B3: 0});
+
+    return coefficients;
 }
 function makePrediction(coefficients, predictions) {
     console.log('coefficients: ', coefficients);
@@ -26,47 +95,17 @@ function makePrediction(coefficients, predictions) {
     predictions.forEach((prediction) => {
         console.log('prediction: ', prediction);
 
+        // START HERE - how handle x1, x2, etc.?
+        //const predictedValue = coefficients.B0
+        // + coefficients.B1 * prediction.x1;
+        // + coefficients.B2 * prediction.x2;
+        // + coefficients.B3 * prediction.x3;
+
         const predictedValue = coefficients.B0 + coefficients.B1 * prediction.x;
 
         console.log('prediction for ' + prediction.x + ' is ' + predictedValue);
         console.log('');  //formatting
     });
-}
-function calculateSimpleLinearRegressionCoefficients(dataSets) {
-    let B0 = 0;
-    let B1 = 0;
-    let error = 0;   //is error cummulative or 0 for each iteration?
-    const alpha = .01;  //aka learning rate
-
-    let ctr = 2;
-    while(true) {
-
-        if (ctr>22) {
-            break;
-        }
-
-        for(let i=0; i<dataSets.length; i++) {
-            const currentDataset = dataSets[i];
-
-            var xNan = isNaN(parseInt(currentDataset.x));
-            var yNan = isNaN(parseInt(currentDataset.y));
-
-            if (!xNan && !yNan) {
-                let p = B0 + B1 * currentDataset.x;
-                error = p - currentDataset.y;
-
-                B0 = B0 - alpha * error;
-                B1 = B1 - alpha * error * currentDataset.x;
-
-                ctr++;
-            }
-        }
-    }
-
-    return {
-        B0,
-        B1,
-    }
 }
 
 module.exports.demoMultipleVariableLinearRegression = demoMultipleVariableLinearRegression;
